a426e67fdcf2afd5d36695084fc7bdb3c1dc5e42
Nam Nguyen <namnguyen@vnsgn-mlsenn.tasernet.com>
2019-06-14 15:53:47 +0700

    Aka

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
       resolve: `gatsby-source-strapi`,
       options: {
         apiURL: process.env.DEPLOY_URL
-          ? process.env.DEPLOY_URL
+          ? "https://YOUR-APP-URL.herokuapp.com"
           : "http://localhost:1337",
         contentTypes: [`article`, `user`],
         queryLimit: 1000,
